f6eb9089bad97cd44752cea17f94a740b99f1d89
Rodrigo <rodrigofantino@gmail.com>
2017-07-17 18:50:55 -0300

    nuevo sistema de array

---
+++ app/src/main/java/com/appsimple/wordable/wordable/canvas/Letter.java
@@ -0,0 +1,8 @@
+package com.appsimple.wordable.wordable.canvas;
+
+/**
+ * Created by Rodrigo on 17/07/2017.
+ */
+
+public class Letter {
+}
---
+++ app/src/main/java/com/appsimple/wordable/wordable/canvas/LettersHandler.java
@@ -0,0 +1,29 @@
+package com.appsimple.wordable.wordable.canvas.Buttons;
+
+import com.appsimple.wordable.wordable.MainActivity;
+import com.appsimple.wordable.wordable.canvas.DrawEndlessMode;
+
+/**
+ * Created by Rodrigo on 06/07/2017.
+ */
+public class LettersHandler {
+
+
+    int yentrearribayabajo = DrawEndlessMode.gridY * 15; //  Es un punto medio entre donde van las letras y donde formo las palabras.
+
+    
+    LettersHandler(int bottom, int left){
+
+        if (bottom < yentrearribayabajo){
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
